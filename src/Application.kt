ee3bfd566dd8bc4f52c2c02ff0e693c30fff94ac
Shengyou Fan <shengyoufan@gmail.com>
2021-02-03 18:09:16 +0800

    add schema and entity

--- src/Application.kt
+++ src/Application.kt
@@ -8,6 +8,10 @@ import io.ktor.http.*
 import com.fasterxml.jackson.databind.*
 import io.ktor.jackson.*
 import io.ktor.features.*
+import org.jetbrains.exposed.dao.IntEntity
+import org.jetbrains.exposed.dao.IntEntityClass
+import org.jetbrains.exposed.dao.id.EntityID
+import org.jetbrains.exposed.dao.id.IntIdTable
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
@@ -31,3 +35,14 @@ fun Application.module(testing: Boolean = false) {
     }
 }
 
+object Tasks : IntIdTable() {
+    val title = varchar("title", 255)
+    val completed = bool("completed").default(false)
+}
+
+class Task(id: EntityID<Int>) : IntEntity(id) {
+    companion object : IntEntityClass<Task>(Tasks)
+
+    var title by Tasks.title
+    var completed by Tasks.completed
+}
