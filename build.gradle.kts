e455548dba2af8bc98709b3e8b6e7a7efc093e36
Shengyou Fan <shengyoufan@gmail.com>
2021-02-03 18:04:57 +0800

    add dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val h2_version: String by project
 
 plugins {
     application
@@ -29,6 +31,10 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-core:$ktor_version")
     implementation("io.ktor:ktor-jackson:$ktor_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("com.h2database:h2:$h2_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
 
